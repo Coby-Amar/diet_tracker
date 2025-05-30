16489aff1f4fa5a16db9a146453f23cbc63c3092
Coby-Amar <cobyamar@gmail.com>
2025-05-30 15:38:25 +0300

    fix org name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.diet_tracker"
+        applicationId = "com.ybocrama.diet_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/diet_tracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/diet_tracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.diet_tracker
+package com.ybocrama.diet_tracker
 
 import io.flutter.embedding.android.FlutterActivity
 
