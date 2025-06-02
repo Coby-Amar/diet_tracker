7a2dc0bc8c999a7a06b3552496c2b53fc592b531
Coby-Amar <cobyamar@gmail.com>
2025-06-02 11:37:30 +0300

    addded error loger

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 import 'package:diet_tracker/resources/provider.dart';
 import 'package:diet_tracker/app.dart';
 
+void logErrorsToFile() async {}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => AppProvider(),
-      child: const Application(),
-    ),
-  );
+  try {
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => AppProvider(),
+        child: const Application(),
+      ),
+    );
+  } catch (e) {
+    final root = await getApplicationDocumentsDirectory();
+    final errorFile = File(join(root.path, 'error.log'));
+    if (!errorFile.existsSync()) {
+      errorFile.createSync();
+    }
+    await errorFile.writeAsString(e.toString());
+  }
 }
